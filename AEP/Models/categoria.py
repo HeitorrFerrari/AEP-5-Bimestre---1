8157010d67837943ab6d8f934f0589c94095d568
Heitor Ferrari <heitorferrari9@gmail.com>
2026-03-25 22:33:32 -0300

    Adicao das categorias de solicitacao

---
+++ AEP/Models/categoria.py
@@ -0,0 +1,12 @@
+from enum import enum 
+
+class categoria(Enum):
+    ILUMINACAO = "Iluminação pública"
+    BURACO = "Buraco nas ruas"
+    PODA = "Podagem de árvores irregulares"
+    SAUDE = "Duvidas ou solicitação de tarefa relacionada a saúde"
+    LIMPEZA = "Limpeza e zeladoria"
+    OUTRO = "Outro"
+
+    def __str__(self):
+        return self.value
